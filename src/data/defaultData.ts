71f52e0b09b5b29393012b10b3675d531807fe15
ritiksahu546 <156475191+ritiksahu546@users.noreply.github.com>
2026-09-07 21:11:00 +0530

    Update defaultData.ts

--- src/data/defaultData.ts
+++ src/data/defaultData.ts
@@ -5,7 +5,7 @@ export const defaultProduct: ProductConfig = {
   title: 'The Money Maker',
   subtitle: 'A Practical Step-by-Step Blueprint for Indian Stock Market Beginners',
   author: 'Abhishek ji',
-  price: 2,
+  price: 295,
   originalPrice: 1120,
   currency: 'INR',
   pages: 200,
